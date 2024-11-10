4bcf8b24f23d2bdbcf9b1f0956749c52c1e9927e
J2H <jh.jeon3233@gmail.com>
2024-11-11 00:35:48 +0900

    v.1.7.0

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import eventlet
 eventlet.monkey_patch()
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room, close_room, rooms, disconnect
+from flask_cors import CORS
 import pymysql
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '비밀번호 설정'
+CORS(app)
 socketio = SocketIO(app, async_mode='eventlet')
 
 db = pymysql.connect(host='43.203.195.110', port=3306, user ='id_j2h', password='Password_j2h!12345', database='chat_app')
@@ -23,27 +25,31 @@ def render_chat_page():
 @app.route('/login/')
 def render_login_page():
     return render_template('login.html')
+
+@app.route('/signup/')
+def render_signup_page():
+    return render_template('signup.html')
+
 @app.route('/login/set/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         login_info = request.form
-
-        # 폼에서 입력한 ID와 비밀번호를 가져옵니다.
-        user_id = login_info['username']  # username은 HTML 폼의 input name
-        password = login_info['password']
+        
+        user_id = login_info['username'] 
 
         sql = "SELECT * FROM users WHERE id=%s"
-        rows_count = cursor.execute(sql, (user_id,))
+        rows_count = cursor.execute(sql, (user_id))
 
         if rows_count > 0:
             user_info = cursor.fetchone()
             print("user info: ", user_info)
+            # 데이터는 user_index, nickname, id, password, email 순서로 정렬
 
-            # 패스워드가 올바른지 확인합니다.
-            is_pw_correct = user_info[3]  # password 열의 인덱스가 3이라고 가정합니다.
+            is_pw_correct = user_info[3]  
             print("password check: ", is_pw_correct)
 
-            return render_template('login.html', info=user_info)
+            # 로그인 성공하면 채팅 페이지로
+            return render_template('chat.html',nickname = user_info[1])
 
         else:
             print('user does not exist')
@@ -52,9 +58,29 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/signup/')
-def render_signup_page():
-    return render_template('signup.html')
+@app.route('/signup/set/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        signup_info = request.form
+
+        email = signup_info['email']
+        user_id = signup_info['id']
+        password = signup_info['password']
+        nickname = signup_info['nickname']
+        
+        sql = """
+            INSERT INTO users (nickname, id, password, email)
+            VALUES (%s, %s, %s, %s);
+        """
+        cursor.execute(sql, (nickname, user_id, password, email))
+        db.commit()
+
+        print(nickname, user_id, password, email)
+
+        return render_template('login.html')
+
+    return render_template('login.html')
+
 
 # 클라이언트 - 서버 연결 이벤트
 # 나중에 쓰지 않을까
@@ -66,6 +92,7 @@ def handle_connect():
 @socketio.event
 def handle_join_room(data):
     room = data.get('number')
+
     name = data.get('name')
     join_room(room)
     emit('system',name+ '님이 채팅방에 참가하셨습니다.', to=room)
@@ -88,31 +115,6 @@ def handle_leave_room(data):
     leave_room(room)
     emit('system', name + '님이 채팅방에서 나갔습니다.', to=room, skip_sid=request.sid)
     print('방에서 나갔습니다.')
-
-
-
-
-
-
-
-# # 회원가입기능
-# # 미완성
-# @socketio.event
-# def signup(data):
-#     email = data.get(email)
-#     id = data.get(id)
-#     password = data.get(password)
-#     nickname = data.get(nickname)
-
-# # 로그인 기능
-# # 미완성
-# @socketio.event
-# def login(data):
-#     id = data.get(id)
-#     password = data.get(password)
-
-
-
 
 
 
